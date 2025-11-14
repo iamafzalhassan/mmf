ddfa598b89957c5a3df3722ac7f69b1e48f63c59
AfzalHassan <afzal.hassan@arimaclanka.com>
2025-11-14 10:49:20 +0530

    Text changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mmf/core/theme/app_theme.dart';
-import 'core/di/injection_container.dart' as di;
-import 'presentation/cubits/main_form_cubit.dart';
-import 'presentation/pages/mahalla_form.dart';
+import 'package:mmf/app.dart';
+import 'package:mmf/core/di/injection_container.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await di.init();
-  runApp(const MyApp());
+  await init();
+  run();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => di.sl<MainFormCubit>(),
-      child: MaterialApp(
-        title: 'Mahalla Members Form',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.lightTheme,
-        home: const MahallaForm(),
-      ),
-    );
-  }
+void run() {
+  runApp(const App());
 }
