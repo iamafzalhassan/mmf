e1a7ba48b35e78b4d3ad5da112f3709486bd9560
AfzalHassan <afzal.hassan@arimaclanka.com>
2025-11-17 13:44:16 +0530

    Form refactored

--- lib/domain/entities/family_member.dart
+++ lib/domain/entities/family_member.dart
@@ -1,55 +1,55 @@
 class FamilyMember {
   final String name;
-  final String nic;
   final String gender;
-  final String civilStatus;
   final String age;
-  final String relationship;
-  final String occupation;
-  final String status;
   final String mobile;
-  final String zakath;
+  final String nic;
+  final String status;
+  final String occupation;
+  final String civilStatus;
+  final String relationship;
   final List<String> schoolEducation;
   final List<String> professionalQualifications;
   final List<String> madarasa;
   final List<String> ulama;
   final List<String> specialNeeds;
+  final String zakath;
 
   FamilyMember({
     required this.name,
-    required this.nic,
     required this.gender,
-    required this.civilStatus,
     required this.age,
-    required this.relationship,
-    required this.occupation,
-    required this.status,
     required this.mobile,
-    required this.zakath,
+    required this.nic,
+    required this.status,
+    required this.occupation,
+    required this.civilStatus,
+    required this.relationship,
     required this.schoolEducation,
     required this.professionalQualifications,
     required this.madarasa,
     required this.ulama,
     required this.specialNeeds,
+    required this.zakath,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'name': name,
-      'nic': nic,
       'gender': gender,
-      'civilStatus': civilStatus,
       'age': age,
-      'relationship': relationship,
-      'occupation': occupation,
-      'status': status,
       'mobile': mobile,
-      'zakath': zakath,
+      'nic': nic,
+      'status': status,
+      'occupation': occupation,
+      'civilStatus': civilStatus,
+      'relationship': relationship,
       'schoolEducation': schoolEducation.join(', '),
       'professionalQualifications': professionalQualifications.join(', '),
       'madarasa': madarasa.join(', '),
       'ulama': ulama.join(', '),
       'specialNeeds': specialNeeds.join(', '),
+      'zakath': zakath,
     };
   }
 }
