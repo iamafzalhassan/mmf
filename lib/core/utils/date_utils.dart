f99e615b865548ebe32e1bc86361bc1011924d6e
AfzalHassan <afzal.hassan@arimaclanka.com>
2026-02-16 20:19:55 +0530

    Random string length shortened

--- lib/core/utils/date_utils.dart
+++ lib/core/utils/date_utils.dart
@@ -1,10 +1,6 @@
 class DateTimeUtils {
   static String generateRefNo() {
-    final now = DateTime.now();
-    final year = now.year;
-    final month = now.month.toString().padLeft(2, '0');
-    final day = now.day.toString().padLeft(2, '0');
-    final random = (DateTime.now().millisecond % 1000).toString().padLeft(3, '0');
-    return 'KJM-$year$month$day-$random';
+    final random = (DateTime.now().millisecond % 1000).toString().padLeft(6, '0');
+    return 'KJM-$random';
   }
 }
